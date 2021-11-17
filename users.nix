e93c6a87a9755c5b9c4d4efca208a50b20a458b3
steve <steve@little-fluffy.cloud>
2021-11-17 17:50:18 +0000

    Initial add of testmachine configs

---
+++ users.nix
@@ -0,0 +1,22 @@
+{ config, lib, pkgs, modulesPath, ... }:
+
+{
+  users.mutableUsers = false;
+  
+  # Add a user.
+  users.users.nixos = {
+    isNormalUser = true;
+  
+    # Add a hashed password, overrides initialPassword.
+    # See below.
+    hashedPassword = "$6$1OvnB1HeKH$9/exwQNwcCUknibmcK2i745uEO/7nJe/53aPAyyvFaadM3zgxSuWcMnQ8NpZZGQegUz2dC5JXgSGk1oCZcjWn.";
+  
+    extraGroups = [ "wheel" ];
+    openssh.authorizedKeys.keys = [
+      # Authorize the SSH public key from 'key.pub'.
+      # Remove this statement if you use password
+      # authentication.
+      (builtins.readFile ./key.pub)
+    ];
+  };
+}
